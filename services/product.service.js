c82cafab4611a4df19bad3600f8ad336369de25a
Eric Felipe Barrera Lopez <erixlopez6@gmail.com>
2023-06-20 22:06:00 -0600

    manejando un pool de conexiones

--- services/product.service.js
+++ services/product.service.js
@@ -1,11 +1,15 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
+const pool = require('../libs/postgres.pool');
+
 class ProductsService {
 
   constructor() {
     this.products = [];
     this.generate();
+    this.pool = pool;
+    this.pool.on('error', (err) => console.error(err));
   }
 
   async generate() {
@@ -53,6 +57,12 @@ class ProductsService {
 
   }
 
+  async find(){
+    const query = 'SELECT * FROM tasks';
+    const rta = await this.pool.query(query);
+    return rta.rows;
+  }
+
   async update(id, data) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
@@ -75,6 +85,7 @@ class ProductsService {
     this.products.splice(index, 1);
     return { id };
   }
+  
 }
 
 module.exports = ProductsService;
